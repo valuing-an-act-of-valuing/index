f33b4f87b4f7f565367748ce4b525c5ae0a3fbda
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-13 02:17:32 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,17 +3,17 @@ async function populate() {
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const indexJson = await response.text();
+  const galleryJson = await response.text();
 
-  const galleryIndex = JSON.parse(indexJson);
+  const galleryIndex = JSON.parse(galleryJson);
   galleryObject(galleryIndex);
 }
 
 function galleryObject(obj) {
   const gallery = document.querySelector('#org .random');
-  const itemsShow = obj.gallery;
+  const itemsGallery = obj.gallery;
 
-  for (const item of itemsShow) {
+  for (const item of itemsGallery) {
     const show = document.createElement('li');
     const showP = document.createElement('p');
     const showSpan = document.createElement('span');
@@ -22,8 +22,8 @@ function galleryObject(obj) {
     show.setAttribute("class", "list_item");
     show.setAttribute("data-appreciate", item.type);
     showP.textContent = item.name;
-    showA.textContent = item.date;
-    topicA.href = item.href;
+    showSpan.textContent = item.date;
+    showA.href = item.href;
 
     show.appendChild(showP);
     show.appendChild(showSpan);
