bff755f0e6e4d6e13eec63d52735d0484863faca
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-12 15:47:18 +0900

    value

--- index.js
+++ index.js
@@ -14,46 +14,6 @@ function indexJson(obj) {
   const titleHead = document.createElement('title');
   titleHead.textContent = ${obj.title};
   head.appendChild(titleHead);
-
-  const authorHead = document.createElement( "meta" );
-  authorHead.setAttribute("name", "author");
-  authorHead.setAttribute("content", obj.author);
-  head.appendChild(authorHead);
-
-  const descriptionHead = document.createElement('meta');
-  descriptionHead.setAttribute("name", "description");
-  descriptionHead.setAttribute("content", obj.description);
-  head.appendChild(descriptionHead);
-
-  const ogTitle = document.createElement('meta');
-  ogTitle.setAttribute("property", "og:title");
-  ogTitle.setAttribute("content", obj.title);
-  head.appendChild(ogTitle);
-
-  const ogDescription = document.createElement('meta');
-  ogDescription.setAttribute("property", "og:description");
-  ogDescription.setAttribute("content", obj.description);
-  head.appendChild(ogDescription);
-
-  const ogSite = document.createElement( "meta" );
-  ogSite.setAttribute("property", "og:site_name");
-  ogSite.setAttribute("content", ${obj.site});
-  head.appendChild(ogSite);
-
-  const ogURL = document.createElement( "meta" );
-  ogURL.setAttribute("property", "og:url");
-  ogURL.setAttribute("content", `${obj.site}${obj.page}`);
-  head.appendChild(ogURL);
-
-  const ogIMG = document.createElement( "meta" );
-  ogIMG.setAttribute("property", "og:image");
-  ogIMG.setAttribute("content", `${obj.site}${obj.page}${obj.card}`);
-  head.appendChild(ogIMG);
-
-  const twitterIMG = document.createElement( "meta" );
-  twitterIMG.setAttribute("name", "twitter:image");
-  twitterIMG.setAttribute("content", `${obj.site}${obj.page}${obj.card}`);
-  head.appendChild(twitterIMG);
 }
 
 populate();
