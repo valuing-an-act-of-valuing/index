b7c85c16e1dc8a89e56fe31776da9cfb3717e952
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-12 15:55:46 +0900

    value

--- index.js
+++ index.js
@@ -3,57 +3,33 @@ async function populate() {
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const json = await response.text();
+  const jsonORG = await response.text();
 
-  const index = JSON.parse(json);
-  indexJson(index);
+  const indexORG = JSON.parse(jsonORG);
+  titleORG(indexORG);
+  objORG(indexORG);
 }
 
-function indexJson(obj) {
+function titleORG(obj) {
   const head = document.querySelector('head');
-  const titleHead = document.createElement('title');
-  titleHead.textContent = ${obj.title};
-  head.appendChild(titleHead);
+  const titleORG = document.createElement('title');
+  titleORG.textContent = `${obj.title} | ${obj.author}`;
+  head.appendChild(titleORG);
 
-  const authorHead = document.createElement( "meta" );
-  authorHead.setAttribute("name", "author");
-  authorHead.setAttribute("content", obj.author);
-  head.appendChild(authorHead);
+  const descriptionORG = document.createElement( "meta" );
+  descriptionORG.setAttribute("name", "description");
+  descriptionORG.setAttribute("content", obj.description);
+  head.appendChild(descriptionORG);
+}
 
-  const descriptionHead = document.createElement('meta');
-  descriptionHead.setAttribute("name", "description");
-  descriptionHead.setAttribute("content", obj.description);
-  head.appendChild(descriptionHead);
+function objORG(obj) {
+  const org = document.querySelector('#org .random');
+  const itemsORG = obj.org;
 
-  const ogTitle = document.createElement('meta');
-  ogTitle.setAttribute("property", "og:title");
-  ogTitle.setAttribute("content", obj.title);
-  head.appendChild(ogTitle);
+  for (const item of itemsORG) {
 
-  const ogDescription = document.createElement('meta');
-  ogDescription.setAttribute("property", "og:description");
-  ogDescription.setAttribute("content", obj.description);
-  head.appendChild(ogDescription);
-
-  const ogSite = document.createElement( "meta" );
-  ogSite.setAttribute("property", "og:site_name");
-  ogSite.setAttribute("content", ${obj.site});
-  head.appendChild(ogSite);
-
-  const ogURL = document.createElement( "meta" );
-  ogURL.setAttribute("property", "og:url");
-  ogURL.setAttribute("content", `${obj.site}${obj.url}`);
-  head.appendChild(ogURL);
-
-  const ogIMG = document.createElement( "meta" );
-  ogIMG.setAttribute("property", "og:image");
-  ogIMG.setAttribute("content", `${obj.site}${obj.url}${obj.image}`);
-  head.appendChild(ogIMG);
-
-  const twitterIMG = document.createElement( "meta" );
-  twitterIMG.setAttribute("name", "twitter:image");
-  twitterIMG.setAttribute("content", `${obj.site}${obj.url}${obj.image}`);
-  head.appendChild(twitterIMG);
+    org.appendChild(itemORG);
+  }
 }
 
 populate();
